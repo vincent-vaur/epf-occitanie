04c2f61de7e28001165b372eb42ced7e588331e1
Vincent Vaur <vaur.vincent@gmail.com>
2019-11-14 22:31:59 +0100

    Mise en place de grunt pour le développement

---
+++ Gruntfile.js
@@ -0,0 +1,65 @@
+const sass = require("node-sass");
+
+const SERVER_PORT = 3000;
+const LIVE_RELOAD_PORT = 35729;
+
+module.exports = function(grunt) {
+  grunt.initConfig({
+    connect: {
+      server: {
+        options: {
+          port: SERVER_PORT,
+          hostname: "localhost",
+          base: "dist",
+          livereload: LIVE_RELOAD_PORT,
+          open: true,
+        },
+      },
+    },
+
+    sass: {
+      options: {
+        implementation: sass,
+        outputStyle: "compressed",
+      },
+      dist: {
+        files: {
+          "dist/styles.css": "src/scss/styles.scss",
+        },
+      },
+    },
+
+    bake: {
+      your_target: {
+        files: {
+          "dist/index.html": "src/index.html",
+        },
+      },
+    },
+
+    watch: {
+      sass: {
+        files: ["src/scss/**.*"],
+        tasks: ["sass"],
+      },
+
+      bake: {
+        files: ["src/**.html"],
+        tasks: ["bake"],
+      },
+
+      livereload: {
+        files: ["dist/**/*.*"],
+        options: { livereload: LIVE_RELOAD_PORT },
+      },
+    },
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks("grunt-sass");
+  grunt.loadNpmTasks("grunt-bake");
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-contrib-connect");
+
+  grunt.registerTask("default", ["sass", "bake", "connect", "watch"]);
+};
